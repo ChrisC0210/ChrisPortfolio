01557a46420403b456dcf463f0a4fee8506f212c
Chris <iformcarechris@gmail.com>
2018-08-20 10:36:25 +0800

    Edit: js

--- js/all.js
+++ js/all.js
@@ -1,33 +1,23 @@
 $(document).ready(function () {
 
   //header color changed
-  $(window).on("scroll", function () {
-    var scrollDistance = $(window).scrollTop();
-    var $header = $(".js-header");
-    if (scrollDistance > 80) {
-      $header.addClass("header--colored");
-    } else {
-      $header.removeClass("header--colored");
-    }
-  })
+    $(window).on("scroll", function () {
+        var scrollDistance = $(window).scrollTop();
+        var $header = $(".js-header");
+        if (scrollDistance > 80) {
+            $header.addClass("header--colored");
+        } else {
+            $header.removeClass("header--colored");
+        }
+    });
   //wow plugin setup
   if (screen.width > 768) {
     new WOW().init();
-  }
+    };
   //FAB
-  $(".fab_icon").click(function () {
-    $("html,body").animate({
-      scrollTop: 0
-    }, 100);
-  });
-  //fancy box plugin options
-  $('[data-fancybox]').fancybox({
-    youtube: {
-      controls: 0,
-      showinfo: 0
-    },
-    vimeo: {
-      color： 'f00'
-    }
-  });
+    $(".fab_icon").click(function () {
+      $("html,body").animate({
+        scrollTop: 0
+      }, 100);
+    });
 })
